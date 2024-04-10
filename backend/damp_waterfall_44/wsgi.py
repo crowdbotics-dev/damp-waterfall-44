fb7f91b4ebda15edd2e59f1618504ce7f731652d
Crowdbotics <engineering@crowdbotics.com>
2024-04-10 13:06:14 +0000

    Initial commit

---
+++ backend/damp_waterfall_44/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for damp_waterfall_44 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'damp_waterfall_44.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "damp-waterfall-44",
+  url: "https://damp-waterfall-44.botics.co",
+  api: "https://damp-waterfall-44.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
